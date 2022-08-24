86874fc6e380464deba5059e81d6b2a3cb5416d8
Festus <akyerefi@Akyerefis-MacBook-Air.local>
2022-08-24 07:53:25 +0000

    Update webpack.config file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.css';
+import './index.css';
 import addTodo from './modules/addTask.js';
 import deleteTodo from './modules/deleteTask.js';
 import editTodo from './modules/editTask.js';
--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,31 @@ module.exports = {
   mode: 'development',
 
   entry: {
-    index: './src/index.js',
+    bundle: path.resolve(__dirname, 'src/index.js'),
   },
 
+  devtool: 'source-map',
   devServer: {
-    static: './dist',
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       title: 'A TODO list',
+      filename: 'index.html',
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
   ],
-
   output: {
-    filename: '[name].js',
+    filename: '[name][contenthash].js',
+    assetModuleFilename: '[name][ext]',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     publicPath: '/',
